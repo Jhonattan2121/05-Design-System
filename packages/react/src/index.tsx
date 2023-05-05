2da59d70b2ee2b95ef84eda8553973ac7d281cca
Jhonattan <jhonattan.ferreira@dlsoftwarehouse.com>
2023-05-05 17:33:08 -0300

    Configurando pacote do React

---
+++ packages/react/src/index.tsx
@@ -0,0 +1,9 @@
+import { colors } from '@ignite-ui/tokens/src/'
+
+export function App() {
+    return (
+        <h1 style={{color: colors.ignite300}}>
+            hello world
+        </h1>
+    )
+}
